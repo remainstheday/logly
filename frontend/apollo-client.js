1b28afe361a63922f286f589422e31a19d5da00e
Trenton Kennedy <trentontri@gmail.com>
2022-01-04 21:42:13 -0600

    configure apollo client

--- frontend/apollo-client.js
+++ frontend/apollo-client.js
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: `https://admin.tovech.com/api/graphql`,
