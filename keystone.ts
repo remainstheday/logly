9c8cd8d26f06fd6e2316f83a2865c260a2eb8a6f
Trenton Kennedy <trentontri@gmail.com>
2022-01-05 19:30:15 -0600

    add session handling to login

--- keystone.ts
+++ keystone.ts
@@ -1,15 +1,28 @@
 require("dotenv").config();
 import { config } from "@keystone-6/core";
-import { withAuth, session } from "./auth";
+import { createAuth } from "@keystone-6/auth";
+import { statelessSessions } from "@keystone-6/core/session";
 import { lists } from "./schema";
+
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
+  },
+});
+
+const session = statelessSessions({
+  // The session secret is used to encrypt cookie data (should be an environment variable)
+  secret: process.env.SESSION_SECRET!,
+});
 
 export default withAuth(
   config({
     db: {
       provider: "postgresql",
-      url: process.env.DATABASE_URL || ``,
-      enableLogging: true,
-      useMigrations: true,
+      url: process.env.DATABASE_URL!,
       idField: { kind: "uuid" },
     },
     graphql: {
