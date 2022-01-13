dfc1c20b51650e36c2f5af719a30de7d64776073
Trenton Kennedy <trentontri@gmail.com>
2022-01-12 21:48:25 -0600

    add art social section to homepage

--- frontend/components/Carousel.js
+++ frontend/components/Carousel.js
@@ -1,80 +1,79 @@
 import React from "react";
 import Link from "next/link";
 
-const Carousel = ({ experiences }) => {
-  const [currentExperience, setCurrentExperience] = React.useState(0);
-  const refs = experiences.reduce((acc, val, i) => {
-    acc[i] = React.createRef();
-    return acc;
-  }, {});
+const Carousel = ({experiences}) => {
+    const [currentExperience, setCurrentExperience] = React.useState(0);
+    const refs = experiences.reduce((acc, val, i) => {
+        acc[i] = React.createRef();
+        return acc;
+    }, {});
 
-  const scrollToImage = (i) => {
-    setCurrentExperience(i);
-    refs[i].current.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "start",
-    });
-  };
+    const scrollToImage = (i) => {
+        setCurrentExperience(i);
+        refs[i].current.scrollIntoView({
+            behavior: "smooth",
+            block: "nearest",
+            inline: "start",
+        });
+    };
 
-  // Some validation for checking the array length could be added if needed
-  const totalImages = experiences.length;
+    // Some validation for checking the array length could be added if needed
+    const totalImages = experiences.length;
 
-  const nextImage = () => {
-    if (currentExperience >= totalImages - 1) {
-      scrollToImage(0);
-    } else {
-      scrollToImage(currentExperience + 1);
-    }
-  };
+    const nextImage = () => {
+        if (currentExperience >= totalImages - 1) {
+            scrollToImage(0);
+        } else {
+            scrollToImage(currentExperience + 1);
+        }
+    };
 
-  const previousImage = () => {
-    if (currentExperience === 0) {
-      scrollToImage(totalImages - 1);
-    } else {
-      scrollToImage(currentExperience - 1);
-    }
-  };
+    const previousImage = () => {
+        if (currentExperience === 0) {
+            scrollToImage(totalImages - 1);
+        } else {
+            scrollToImage(currentExperience - 1);
+        }
+    };
 
-  // Tailwind styles. Most importantly notice position absolute, this will sit relative to the carousel's outer div.
-  const arrowStyle =
-    "absolute text-white text-2xl z-10 bg-black h-10 w-10 rounded-full opacity-75 flex items-center justify-center";
+    // Tailwind styles. Most importantly notice position absolute, this will sit relative to the carousel's outer div.
+    const arrowStyle =
+        "absolute text-white text-2xl z-10 bg-black h-10 w-10 rounded-full opacity-75 flex items-center justify-center";
 
-  const sliderControl = (isLeft) => (
-    <button
-      type="button"
-      onClick={isLeft ? previousImage : nextImage}
-      className={`${arrowStyle} ${isLeft ? "left-2" : "right-2"}`}
-      style={{ top: "40%" }}
-    >
+    const sliderControl = (isLeft) => (
+        <button
+            type="button"
+            onClick={isLeft ? previousImage : nextImage}
+            className={`${arrowStyle} ${isLeft ? "left-2" : "right-2"}`}
+            style={{top: "40%"}}
+        >
       <span role="img" aria-label={`Arrow ${isLeft ? "left" : "right"}`}>
         {isLeft ? "◀" : "▶"}
       </span>
-    </button>
-  );
+        </button>
+    );
 
-  return (
-    <div className="w- flex justify-center">
-      <div className="p-12 flex justify-center w-screen md:w-1/2 items-center">
-        <div className="relative w-full">
-          <div className="carousel">
-            {sliderControl(true)}
-            {experiences.map((experience, index) => (
-                <div key={index} className="w-full flex-shrink-0">
-                  <Link href={`/experiences${experience.slug}`} passHref>
-                    <>
-                    <img src={`https://admin.logly.world${experience.poster.url}`} ref={refs[index]} className="w-full object-contain" />
-                    <strong>{experience.title}</strong>
-                    </>
-                  </Link>
+    return (
+        <div className="w-full flex justify-center">
+            <div className="flex justify-center items-center">
+                <div className="relative w-full">
+                    <div className="carousel">
+                        {sliderControl(true)}
+                        {experiences.map((experience, index) => (
+                            <Link key={index} href={`/experiences${experience.slug}`} passHref>
+                                <div  className="w-full flex-shrink-0">
+                                    <img src={`https://admin.logly.world${experience.poster.url}`} ref={refs[index]}
+                                         className="w-full object-contain"/>
+                                    <strong>{experience.title}</strong>
+                                </div>
+                            </Link>
+                        ))}
+                        {sliderControl()}
+                    </div>
                 </div>
-            ))}
-            {sliderControl()}
-          </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Carousel;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,56 +1,71 @@
-import Head from "next/head";
-import Link from "next/link";
+import Link from 'next/link'
 import Image from "next/image";
 import Header from "../components/Header";
 import Carousel from "../components/Carousel";
 
-import { getAllExperiences, getExperienceBySlug } from "../lib/api";
+import {getAllExperiences} from "../lib/api";
 
-export default function Home({ experiences = [] }) {
-  return (
-    <div className="max-w-7xl mx-auto">
-      <Header />
-      <main>
-        <p className="uppercase text-center">Welcome to</p>
-        <h1 className="text-3xl leading-3 text-center font-bold mt-0 pt-0">
-          <br /> LOGLY
-        </h1>
+export default function Home({experiences = []}) {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <Header/>
+            <main>
+                <p className="uppercase text-center">Welcome to</p>
+                <h1 className="text-3xl leading-3 text-center font-bold mt-0 pt-0">
+                    <br/> LOGLY
+                </h1>
 
-        <div className="flex relative max-w-full w-full h-96 my-16">
-          <Image
-            src="/logly.png"
-            className="w-1/3"
-            alt="logly-logo"
-            width="50"
-            height="50"
-          />
+                <div className="flex relative max-w-full w-full h-96 my-16">
+                    <Image
+                        src="/stock-museum-1.jpg"
+                        className="w-full"
+                        layout="fill"
+                    />
+                </div>
+
+                <section className="container mx-auto mt-4 px-4">
+                    <h3 className="px-4">
+                        Brief intro Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Nobis aut impedit, minus doloribus cumque nulla eum molestiae
+                        eligendi obcaecati. Ratione magnam repudiandae dolorum minima
+                        aspernatur nostrum sit iusto rem iure.
+                    </h3>
+                </section>
+
+                <section className="container mt-4 px-4 mt-10 mx-auto">
+                    <h3 className="pb-3">Pick an Experience</h3>
+                    <hr/>
+                    <div className="w-full mt-4">
+                        <Carousel experiences={experiences}/>
+                        <Link href={`/experiences`} passHref>See all experiences +</Link>
+                    </div>
+                </section>
+
+                <section className="container mt-4 px-4 mt-10 mx-auto">
+                    <h3 className="pb-3">Art Social</h3>
+                    <hr/>
+                    <div className="w-full mt-4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aut autem
+                            blanditiis consectetur doloribus eius eos, et ipsam itaque laboriosam magnam minus
+                            molestiae, numquam, officiis possimus provident sed. Ipsam.</p>
+                        <Link href={`/`} passHref>Discover Art Social +</Link>
+                    </div>
+                </section>
+
+                <footer>
+                    <p>My account</p>
+                    <p>About</p>
+                    <p>Legal stuff</p>
+                </footer>
+            </main>
         </div>
-
-        <section className="container mx-auto mt-4 px-4">
-          <h3 className="px-4">
-            Brief intro Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Nobis aut impedit, minus doloribus cumque nulla eum molestiae
-            eligendi obcaecati. Ratione magnam repudiandae dolorum minima
-            aspernatur nostrum sit iusto rem iure.
-          </h3>
-        </section>
-
-        <section className="container  mt-4 px-4 mt-10 mx-auto ">
-          <h3 className="pb-3">Pick an Experience</h3>
-          <hr />
-          <div className="experience-slider max-w-md mt-5 w-5/6">
-            <Carousel experiences={experiences} />
-          </div>
-        </section>
-      </main>
-    </div>
-  );
+    );
 }
 
 export async function getStaticProps() {
-  const experiences = await getAllExperiences();
+    const experiences = await getAllExperiences();
 
-  return {
-    props: { experiences },
-  };
+    return {
+        props: {experiences},
+    };
 }
