353801a86cc254c38faddd0edb80522e21cb5d2e
Trenton Kennedy <trentontri@gmail.com>
2022-02-15 23:08:34 -0600

    Why does Tyler make me work this late?

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
-import { slide as Menu } from "react-burger-menu";
+import { useState } from "react";
 
 export default function Header() {
+  const [mobileMenu, updateMobileMenu] = useState(false);
   return (
     <>
       <Head>
@@ -19,27 +20,59 @@ export default function Header() {
         />
       </Head>
 
-      <header className="flex justify-between py-4 mx-3 max-w-4xl mx-auto mx-1 md:mx-auto">
-        <Image
-          src="/logly.png"
-          className="w-1/3"
-          alt="logly-logo"
-          width="100"
-          height="50"
-        />
-
-        <div className="relative mr-3 md:mr-0">
-          <Menu right width={"350px"}>
-            <Link href={`/`} passHref>
-              Museum Information
-            </Link>
-            <Link href={`/experiences`} passHref>
-              Experiences
-            </Link>
-            <Link href={`/art-social`} passHref>
-              Art Social
-            </Link>
-          </Menu>
+      <header className="py-4 max-w-4xl mx-auto md:mx-auto">
+        <div className="flex flex-wrap py-2">
+          <div className="w-full">
+            <nav className="relative flex  items-center justify-between py-3 w-full rounded">
+              <div className="w-full relative mx-auto flex flex-wrap items-center justify-end text-right">
+                <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                  <Link href={`/`} passHref>
+                    <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white">
+                      <Image
+                        src="/logly.png"
+                        className="w-1/3"
+                        alt="logly-logo"
+                        width="100"
+                        height="50"
+                      />
+                    </a>
+                  </Link>
+                  <button
+                    className="cursor-pointer text-xl mr-6 leading-none py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                    type="button"
+                    onClick={() => updateMobileMenu(!mobileMenu)}
+                  >
+                    <span className="block relative w-8 h-0.5 rounded-sm bg-black"></span>
+                    <span className="block relative w-8 h-0.5 rounded-sm bg-black mt-1"></span>
+                    <span className="block relative w-8 h-0.5 rounded-sm bg-black mt-1"></span>
+                  </button>
+                </div>
+                <div
+                  className={`${
+                    mobileMenu ? "visible" : "invisible"
+                  } flex pr-6 bg-white absolute pt-6 w-1/2 sm:w-1/3 h-screen right-0 top-12 z-10 lg:visible lg:static lg:pt-0 lg:w-auto lg:h-auto lg:pr-0 lg:flex-grow`}
+                >
+                  <ul className="flex flex-col lg:flex-row list-none ml-auto text-right text-right space-x-12">
+                    <li className="py-2 flex justify-end text-sm uppercase font-bold leading-snug text-black hover:opacity-75 ">
+                      <Link href={`/`} passHref>
+                        <a className="text-right">Museum Information</a>
+                      </Link>
+                    </li>
+                    <li className="py-2 flex justify-end text-sm uppercase font-bold leading-snug text-black hover:opacity-75">
+                      <Link href={`/experiences`} passHref>
+                        <a>Experiences</a>
+                      </Link>
+                    </li>
+                    <li className="py-2 pr-0 flex justify-end text-sm uppercase font-bold leading-snug text-black hover:opacity-75 block ">
+                      <Link href={`/art-social`} passHref>
+                        <a className="text-right block">Art Social</a>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
         </div>
       </header>
     </>
