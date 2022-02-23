803a3e4861416498a2e0440841d7b2d9e624003b
Trenton Kennedy <trentontri@gmail.com>
2022-02-23 07:52:07 -0600

    testing

--- schema.ts
+++ schema.ts
@@ -18,6 +18,7 @@ export const cloudinary = {
   apiKey: process.env.CLOUDINARY_KEY || "",
   apiSecret: process.env.CLOUDINARY_SECRET || "",
   folder: "experiences",
+  resource_type: "auto",
 };
 
 export const lists: Lists = {
