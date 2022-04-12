0a3c5d8e7e8e7638072f435f71b0d00c40e62c58
Trenton Kennedy <trentontri@gmail.com>
2022-04-12 00:04:20 -0500

    update api url

--- frontend/lib/apollo-client.js
+++ frontend/lib/apollo-client.js
@@ -2,8 +2,7 @@ import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
     ssrMode: true,
-    uri: "http://localhost:3000/api/graphql",
-    // uri: API_URL,
+    uri: process.env.API_URL,
     cache: new InMemoryCache(),
 });
 
