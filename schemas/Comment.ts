7fff5a92a9388338bdc9dcd88ffc3823be3f1eea
Trenton Kennedy <trentontri@gmail.com>
2022-04-24 15:34:28 -0500

    include image field in comment database schema

--- schemas/Comment.ts
+++ schemas/Comment.ts
@@ -1,21 +1,26 @@
 import { list } from "@keystone-6/core";
 import { text } from "@keystone-6/core/fields";
+import { cloudinaryImage } from "@keystone-6/cloudinary";
+import { cloudinary } from "../cloudinary";
 
 export const Comment = list({
   fields: {
     comment: text({
-     ui: {
-         createView: {
-             fieldMode: ({ session, context }) => 'hidden',
-         },
-         itemView: {
-             fieldMode: ({ session, context, item }) => 'read',
-         },
-         listView: {
-             fieldMode: ({ session, context }) => 'read',
-         },
-
-     }
+      ui: {
+        createView: {
+          fieldMode: ({ session, context }) => "hidden",
+        },
+        itemView: {
+          fieldMode: ({ session, context, item }) => "read",
+        },
+        listView: {
+          fieldMode: ({ session, context }) => "read",
+        },
+      },
+    }),
+    images: cloudinaryImage({
+      cloudinary,
+      label: "Comment",
     }),
   },
 });
