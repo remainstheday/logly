fd9d5f16c02696b231d5a1cea72b187ef240485d
Trenton Kennedy <trentontri@gmail.com>
2022-06-26 23:48:42 -0500

    add convertkit email capture form to marketing page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -69,16 +69,16 @@ export default function IndexPage() {
                           <a className="text-right">Contact</a>
                         </Link>
                       </li>
-                      <li className="py-2 flex justify-end text-sm uppercase font-bold leading-snug text-black hover:opacity-75">
-                        <Link href={`/register`} passHref>
-                          <a>Signup/Login</a>
-                        </Link>
-                      </li>
-                      <li className="py-2 pr-0 flex justify-end text-sm uppercase font-bold leading-snug text-black hover:opacity-75 block ">
-                        <Link href={`/pricing`} passHref>
-                          <a className="text-right block">Pricing</a>
-                        </Link>
-                      </li>
+                      {/*<li className="py-2 flex justify-end text-sm uppercase font-bold leading-snug text-black hover:opacity-75">*/}
+                      {/*  <Link href={`/register`} passHref>*/}
+                      {/*    <a>Signup/Login</a>*/}
+                      {/*  </Link>*/}
+                      {/*</li>*/}
+                      {/*<li className="py-2 pr-0 flex justify-end text-sm uppercase font-bold leading-snug text-black hover:opacity-75 block ">*/}
+                      {/*  <Link href={`/pricing`} passHref>*/}
+                      {/*    <a className="text-right block">Pricing</a>*/}
+                      {/*  </Link>*/}
+                      {/*</li>*/}
                     </ul>
                   </div>
                 </div>
@@ -87,30 +87,75 @@ export default function IndexPage() {
           </div>
         </header>
 
-        <div className="mt-24 mx-auto md:mx-auto">
+        <div className="mt-24 mx-auto md:mx-auto ">
           <main>
-            <h1 className="text-5xl text-center">
-              Logly is a tool for self representation.
-            </h1>
-            <Link href="/register" passHref>
-              <a className="get-started-btn">Get Started</a>
-            </Link>
-            <p>
-              Logly is{" "}
-              <strong>
-                more than a digital guide. Its an interactive experience.
-              </strong>{" "}
-              Your Visitors can become active participants.
-            </p>
-            <p>
-              Who are your visitors? What inspires them? On Logly they can share
-              their stories with you.
-            </p>
+            <div className="w-1/2 mx-auto">
+              <h1 className="text-5xl text-center mb-10">
+                Logly is a tool for self representation.
+              </h1>
+              <p>
+                Logly is{" "}
+                <strong>
+                  more than a digital guide. Its an interactive experience.
+                </strong>{" "}
+                Your Visitors can become active participants.
+              </p>
+              <p>
+                Who are your visitors? What inspires them? On Logly they can
+                share their stories with you.
+              </p>
+              {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+              <script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
+              <form
+                action="https://app.convertkit.com/forms/3337562/subscriptions"
+                className="seva-form formkit-form"
+                method="post"
+                data-sv-form="3337562"
+                data-uid="32f877138b"
+                data-format="inline"
+                data-version="5"
+                data-options='{"settings":{"after_subscribe":{"action":"message","success_message":"Thank you for subscribing!","redirect_url":""},"analytics":{"google":null,"fathom":null,"facebook":null,"segment":null,"pinterest":null,"sparkloop":null,"googletagmanager":null},"modal":{"trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"powered_by":{"show":true,"url":"https://convertkit.com/features/forms?utm_campaign=poweredby&amp;utm_content=form&amp;utm_medium=referral&amp;utm_source=dynamic"},"recaptcha":{"enabled":false},"return_visitor":{"action":"hide","custom_content":""},"slide_in":{"display_in":"bottom_right","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"sticky_bar":{"display_in":"top","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15}},"version":"5"}'
+                min-width="400 500 600 700 800"
+              >
+                <div data-style="clean">
+                  <ul
+                    className="formkit-alert formkit-alert-error"
+                    data-element="errors"
+                    data-group="alert"
+                  ></ul>
+                  <div
+                    data-element="fields"
+                    data-stacked="false"
+                    className="seva-fields formkit-fields flex flex-inline justify-center mt-5"
+                  >
+                    <div className="formkit-field w-1/2">
+                      <input
+                        className="formkit-input shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        name="email_address"
+                        aria-label="Email Address"
+                        placeholder="Email Address"
+                        required=""
+                        type="email"
+                      />
+                    </div>
+                    <button
+                      data-element="submit"
+                      className="formkit-submit formkit-submit ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    >
+                      <div className="formkit-spinner">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                      </div>
+                      <span className="">Subscribe</span>
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
 
             <section className="mt-40 mx-auto max-w-4xl">
-              <h2 className="text-3xl text-center">
-                👇 What your visitors see 👇
-              </h2>
+              <h2 className="text-3xl text-center">What your visitors see</h2>
               <div className="flex flex-inline place-content-between mt-10 mx-auto inline-screenshots">
                 <div className="w-60 screenshot-container">
                   <Image
@@ -140,7 +185,7 @@ export default function IndexPage() {
             </section>
 
             <section className="mt-40 mx-auto max-w-4xl">
-              <h2 className="text-3xl text-center">👇 What you see 👇</h2>
+              <h2 className="text-3xl text-center">What you see</h2>
               <div className="flex flex-inline items-center split-container">
                 <Image
                   src="/images/desktop-screenshot-admin-experiences.png"
