cfa8e2739cbcb889300e53fb29c21321e670bb53
Trenton Kennedy <trentontri@gmail.com>
2022-06-29 10:26:39 -0500

    cleanup temp homepage

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -65,7 +65,7 @@ export default function IndexPage() {
 
         <div className="mt-24 mx-auto md:mx-auto ">
           <main>
-            <div className="w-1/2 mx-auto">
+            <div className="w-8/12 mx-auto">
               <h1 className="text-5xl text-center mb-10">
                 Logly is a tool for self representation.
               </h1>
@@ -74,158 +74,65 @@ export default function IndexPage() {
                 <strong>
                   more than a digital guide. Its an interactive experience.
                 </strong>{" "}
-                Your Visitors can become active participants.
+                Your Visitors can become active participants. Who are your
+                visitors? What inspires them? On Logly they can share their
+                stories with you.
               </p>
-              <p>
-                Who are your visitors? What inspires them? On Logly they can
-                share their stories with you.
-              </p>
-              {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-              <script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
-              <form
-                action="https://app.convertkit.com/forms/3337562/subscriptions"
-                className="seva-form formkit-form"
-                method="post"
-                data-sv-form="3337562"
-                data-uid="32f877138b"
-                data-format="inline"
-                data-version="5"
-                data-options='{"settings":{"after_subscribe":{"action":"message","success_message":"Thank you for subscribing!","redirect_url":""},"analytics":{"google":null,"fathom":null,"facebook":null,"segment":null,"pinterest":null,"sparkloop":null,"googletagmanager":null},"modal":{"trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"powered_by":{"show":true,"url":"https://convertkit.com/features/forms?utm_campaign=poweredby&amp;utm_content=form&amp;utm_medium=referral&amp;utm_source=dynamic"},"recaptcha":{"enabled":false},"return_visitor":{"action":"hide","custom_content":""},"slide_in":{"display_in":"bottom_right","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"sticky_bar":{"display_in":"top","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15}},"version":"5"}'
-                min-width="400 500 600 700 800"
-              >
-                <div data-style="clean">
-                  <ul
-                    className="formkit-alert formkit-alert-error"
-                    data-element="errors"
-                    data-group="alert"
-                  ></ul>
-                  <div
-                    data-element="fields"
-                    data-stacked="false"
-                    className="seva-fields formkit-fields flex flex-inline justify-center mt-5"
-                  >
-                    <div className="formkit-field w-1/2">
-                      <input
-                        className="formkit-input shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        name="email_address"
-                        aria-label="Email Address"
-                        placeholder="Email Address"
-                        required=""
-                        type="email"
-                      />
-                    </div>
-                    <button
-                      data-element="submit"
-                      className="formkit-submit formkit-submit ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+
+              <div className="mt-12">
+                {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+                <script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
+                <form
+                  action="https://app.convertkit.com/forms/3337562/subscriptions"
+                  className="seva-form formkit-form"
+                  method="post"
+                  data-sv-form="3337562"
+                  data-uid="32f877138b"
+                  data-format="inline"
+                  data-version="5"
+                  data-options='{"settings":{"after_subscribe":{"action":"message","success_message":"Thank you for subscribing!","redirect_url":""},"analytics":{"google":null,"fathom":null,"facebook":null,"segment":null,"pinterest":null,"sparkloop":null,"googletagmanager":null},"modal":{"trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"powered_by":{"show":true,"url":"https://convertkit.com/features/forms?utm_campaign=poweredby&amp;utm_content=form&amp;utm_medium=referral&amp;utm_source=dynamic"},"recaptcha":{"enabled":false},"return_visitor":{"action":"hide","custom_content":""},"slide_in":{"display_in":"bottom_right","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"sticky_bar":{"display_in":"top","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15}},"version":"5"}'
+                  min-width="400 500 600 700 800"
+                >
+                  <div data-style="clean">
+                    <ul
+                      className="formkit-alert formkit-alert-error"
+                      data-element="errors"
+                      data-group="alert"
+                    ></ul>
+                    <div
+                      data-element="fields"
+                      data-stacked="false"
+                      className="seva-fields formkit-fields flex flex-inline justify-center mt-5"
                     >
-                      <div className="formkit-spinner">
-                        <div></div>
-                        <div></div>
-                        <div></div>
+                      <div className="formkit-field w-1/2">
+                        <input
+                          className="formkit-input shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                          name="email_address"
+                          aria-label="Email Address"
+                          placeholder="Email Address"
+                          required=""
+                          type="email"
+                        />
                       </div>
-                      <span className="">Subscribe</span>
-                    </button>
+                      <button
+                        data-element="submit"
+                        className="formkit-submit formkit-submit ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      >
+                        <div className="formkit-spinner">
+                          <div></div>
+                          <div></div>
+                          <div></div>
+                        </div>
+                        <span className="">Subscribe</span>
+                      </button>
+                    </div>
                   </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
-
-            <section className="mt-40 mx-auto max-w-4xl">
-              <h2 className="text-3xl text-center">What your visitors see</h2>
-              <div className="flex flex-inline place-content-between mt-10 mx-auto inline-screenshots">
-                <div className="w-60 screenshot-container">
-                  <Image
-                    src="/images/mobile-screenshot-homepage.png"
-                    width="227"
-                    height="342"
-                  />
-                  <p>A digital tool with your branding</p>
-                </div>
-                <div className="w-60 screenshot-container">
-                  <Image
-                    src="/images/mobile-screenshot-homepage.png"
-                    width="227"
-                    height="342"
-                  />
-                  <p>Tour guides with interpretative materials</p>
-                </div>
-                <div className="w-60 screenshot-container">
-                  <Image
-                    src="/images/mobile-screenshot-homepage.png"
-                    width="227"
-                    height="342"
-                  />
-                  <p>A space to share their thoughts + connect with others</p>
-                </div>
-              </div>
-            </section>
-
-            <section className="mt-40 mx-auto max-w-4xl">
-              <h2 className="text-3xl text-center">What you see</h2>
-              <div className="flex flex-inline items-center split-container">
-                <Image
-                  src="/images/desktop-screenshot-admin-experiences.png"
-                  width="480"
-                  height="460"
-                />
-                <div>
-                  <h3 className="text-2xl font-bold">Create Guides</h3>
-                  <p>
-                    Build Educational and engaging experiences for your
-                    community
-                  </p>
-                </div>
-              </div>
-              <div className="flex flex-inline items-center split-container">
-                <div>
-                  <h3 className="text-2xl font-bold">
-                    Upload Interpretive Materials
-                  </h3>
-                  <p>
-                    You can upload text, audio, and images to your experiences
-                  </p>
-                </div>
-                <Image
-                  src="/images/desktop-screenshot-admin-experiences.png"
-                  width="480"
-                  height="460"
-                />
-              </div>
-
-              <div className="flex flex-inline items-center split-container">
-                <Image
-                  src="/images/desktop-screenshot-admin-experiences.png"
-                  width="480"
-                  height="460"
-                />
-                <div>
-                  <h3 className="text-2xl font-bold">
-                    Manage and Share Comments
-                  </h3>
-                  <p>See what your community thinks and decide what to share</p>
-                </div>
-              </div>
-            </section>
-
-            {/*<section>*/}
-            {/*  <h2>Pricing</h2>*/}
-            {/*  <div className="pricing">*/}
-            {/*    <h3>Logly Premium</h3>*/}
-            {/*    <ul>*/}
-            {/*      <li>-Unlimited guides</li>*/}
-            {/*    </ul>*/}
-            {/*  </div>*/}
-            {/*</section>*/}
-
-            {/*<section>*/}
-            {/*  <h3>FAQ</h3>*/}
-            {/*  <ul>*/}
-            {/*    <li>What is Logly?</li>*/}
-            {/*  </ul>*/}
-            {/*</section>*/}
           </main>
         </div>
-        <footer className={`w-full px-3 py-10 mt-20 bg-zinc-900 text-white`}>
+        <footer className="w-full fixed bottom-0 px-3 py-10 mt-20 bg-zinc-900 text-white">
           <div className="flex justify-between max-w-4xl mx-auto">
             <ul>
               <li>
