0245eee750c3dee931bf17f3d095b8b737d5d459
Trenton Kennedy <trentontri@gmail.com>
2022-07-04 13:19:49 -0500

    add button to generate qr codes

--- fields/qrcode/view.tsx
+++ fields/qrcode/view.tsx
@@ -25,19 +25,21 @@ export const Field = ({
       <Global styles={Styles} />
       <div className="qr-codes-container">
         <h3>{field.label}</h3>
+        <Button tone="active" onClick={() => window.location.reload()}>
+          Generate
+        </Button>
         {QRCodes.length > 0 && (
-          <Button tone="active" onClick={handlePrint}>
-            Print
+          <Button
+            tone="active"
+            style={{ marginLeft: 25 }}
+            onClick={handlePrint}
+          >
+            Download
           </Button>
         )}
 
         <div className="qr-codes">
-          {QRCodes.length <= 0 && (
-            <i>
-              QR Codes will automatically generate once there are related
-              experiences.
-            </i>
-          )}
+          {QRCodes.length <= 0 && <i>QR Codes require related experiences.</i>}
           {QRCodes &&
             QRCodes.length > 0 &&
             QRCodes.map(
