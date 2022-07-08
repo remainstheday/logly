4df19644b7fc1099ce58a4497bbc9e8c8eef42b1
Trenton Kennedy <trentontri@gmail.com>
2022-07-07 23:42:45 -0500

    update faq page

--- frontend/pages/media.js
+++ frontend/pages/media.js
@@ -2,22 +2,25 @@ import React from "react";
 import Link from "next/link";
 import PublicFooter from "components/PublicFooter";
 import PublicHeader from "components/PublicHeader";
+import Section from "components/Section";
 
 export default function Media() {
   return (
     <>
       <PublicHeader />
-      <div className="mt-24 mx-auto md:mx-auto w-8/12 mx-auto">
-        <h1 className="text-5xl text-center mb-10">Media</h1>
-        <p>
-          <strong>February 15, 2022: </strong>
-          <Link href="https://medium.com/@louisapotthast/digital-futures-x-museums-how-does-digital-technology-impact-the-way-we-engage-with-art-2e4f33975b4d">
-            <a className="text-sky-500">
-              Digital Futures x Museums: How does digital technology impact the
-              way we engage with art?
-            </a>
-          </Link>
-        </p>
+      <div className="max-w-4xl mx-auto min-h-screen md:mx-auto">
+        <Section>
+          <h1 className="text-5xl text-center mb-10">Media</h1>
+          <p>
+            <strong>February 15, 2022: </strong>
+            <Link href="https://medium.com/@louisapotthast/digital-futures-x-museums-how-does-digital-technology-impact-the-way-we-engage-with-art-2e4f33975b4d">
+              <a className="text-sky-500">
+                Digital Futures x Museums: How does digital technology impact
+                the way we engage with art?
+              </a>
+            </Link>
+          </p>
+        </Section>
       </div>
       <PublicFooter />
     </>
