dc3a6e5edc623b24a031d71790eb7e272e814784
Trenton Kennedy <trentontri@gmail.com>
2022-07-17 11:00:06 -0500

    revert commit

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,9 +2,6 @@ const withTM = require("next-transpile-modules")(["pintura"]);
 
 module.exports = withTM({
   reactStrictMode: true,
-  experimental: {
-    isrMemoryCacheSize: 0,
-  },
   images: {
     loader: "custom",
     domains: [
