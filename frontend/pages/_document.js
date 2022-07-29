772ee7d4ced0139c9e56faf94f63d3229793d3c0
Trenton K <trentonk@Trentons-MacBook-Pro-2.local>
2022-07-28 21:01:33 -0700

    fix analytics

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -5,6 +5,21 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', { page_path: window.location.pathname });
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
