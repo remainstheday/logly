6f26e5d19164fa57718bc7302307c7f98976c640
Trenton K <trentonk@Trentons-MacBook-Pro-2.local>
2022-08-29 11:48:18 -0500

    more analytics updates

--- apps/frontend/pages/_app.js
+++ apps/frontend/pages/_app.js
@@ -1,42 +1,43 @@
-import { ApolloProvider } from "@apollo/client";
-import { useApollo } from "apollo/apollo-client";
+import {ApolloProvider} from "@apollo/client";
+import {useApollo} from "apollo/apollo-client";
 
+import Head from "next/head";
 import Script from "next/script";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import "styles/globals.css";
 import "styles/imports.css";
-import ClientOnly from "../components/ClientOnly";
 
-export default function App({ Component, pageProps }) {
-  const apolloClient = useApollo(pageProps);
-  const [gtagDate, setGtagDate] = useState(new Date());
+export default function App({Component, pageProps}) {
+    const apolloClient = useApollo(pageProps);
+    const [gtagDate, setGtagDate] = useState(new Date());
 
-  useEffect(() => setGtagDate(new Date()), []);
-  return (
-    <ApolloProvider client={apolloClient}>
-      <Script
-        strategy="beforeInteractive"
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=UA-212210451-1`}
-      />
+    useEffect(() => setGtagDate(new Date()), []);
+    return (
+        <ApolloProvider client={apolloClient}>
 
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="beforeInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=UA-212210451-1`}
-      />
-      <ClientOnly>
-        <Script id="gtag-init" strategy="beforeInteractive">
-          {`
+            <Script
+                strategy="beforeInteractive"
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=UA-212210451-1`}
+            />
+
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <Script
+                strategy="beforeInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=UA-212210451-1`}
+            />
+
+            <Script id="gtag-init" strategy="beforeInteractive">
+                {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', ${gtagDate});
         
             gtag('config', 'UA-212210451-1');
         `}
-        </Script>
-      </ClientOnly>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+            </Script>
+
+            <Component {...pageProps} />
+        </ApolloProvider>
+    );
 }
