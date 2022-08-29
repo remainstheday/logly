cc3202021038fb8cca35a2cb19f282d81a8bc919
remainstheday <trentontri@gmail.com>
2022-08-29 14:34:51 -0500

    add google analytics component

--- apps/frontend/pages/_app.js
+++ apps/frontend/pages/_app.js
@@ -1,40 +1,17 @@
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "apollo/apollo-client";
-
-import Script from "next/script";
-import { useEffect, useState } from "react";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 import "styles/globals.css";
 import "styles/imports.css";
 
+// By default, scripts are loaded using the afterInteractive strategy,
+// which means they are injected on the client-side and will run after hydration.
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps);
-  const [gtagDate, setGtagDate] = useState(new Date());
 
-  useEffect(() => setGtagDate(new Date()), []);
   return (
     <ApolloProvider client={apolloClient}>
-      <Script
-        strategy="beforeInteractive"
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=UA-212210451-1`}
-      />
-
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="beforeInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=UA-212210451-1`}
-      />
-
-      <Script id="gtag-init" strategy="beforeInteractive">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', ${gtagDate});
-        
-            gtag('config', 'UA-212210451-1');
-        `}
-      </Script>
-
+      <GoogleAnalytics trackPageViews gaMeasurementId="UA-212210451-1" />
       <Component {...pageProps} />
     </ApolloProvider>
   );
