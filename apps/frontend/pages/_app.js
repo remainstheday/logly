b38ec4fc27c41d5124b67122d5580a5776cbd9b5
Trenton K <trentonk@Trentons-MBP-2.attlocal.net>
2022-08-29 00:15:47 -0500

    make analytics beforeInteractive

--- apps/frontend/pages/_app.js
+++ apps/frontend/pages/_app.js
@@ -1,53 +1,35 @@
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "apollo/apollo-client";
-import * as gtag from "config/gtag";
-import { useRouter } from "next/router";
+
 import Script from "next/script";
-import { useEffect, useState } from "react";
 import "styles/globals.css";
 import "styles/imports.css";
 
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps);
-  const router = useRouter();
-  const [formattedDate, setFormattedDate] = useState(null);
-
-  useEffect(() => {
-    setFormattedDate(new Date());
-
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    router.events.on("hashChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-      router.events.off("hashChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
 
   return (
     <ApolloProvider client={apolloClient}>
+      <Script
+        strategy="beforeInteractive"
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=UA-212210451-1`}
+      />
+
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        strategy="beforeInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=UA-212210451-1`}
       />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      <Script id="gtag-init" strategy="beforeInteractive">
+        {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
-            gtag('js', ${formattedDate});
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: 'window.location.pathname',
-            });
-          `,
-        }}
-      />
+            gtag('js', new Date());
+        
+            gtag('config', 'UA-212210451-1');
+        `}
+      </Script>
       <Component {...pageProps} />
     </ApolloProvider>
   );
--- apps/frontend/pages/_document.js
+++ apps/frontend/pages/_document.js
@@ -1,35 +1,14 @@
 import { Head, Html, Main, NextScript } from "next/document";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
 export default function Document() {
   const router = useRouter();
-  const [formattedDate, setFormattedDate] = useState(null);
 
-  useEffect(() => {
-    console.log(router);
-    setFormattedDate(new Date());
-  }),
-    [];
   return (
     <Html>
-      <Head>
+      <Head title="logly world">
+        <title>logly.world</title>
         <link rel="icon" href="/favicon.ico" />
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-        />
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', ${formattedDate});
-              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', { page_path: 'window.location.pathname'});
-            `,
-          }}
-        />
       </Head>
       <body>
         <Main />
