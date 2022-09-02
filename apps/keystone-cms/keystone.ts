dbb444360b1089aae1ae0d4156d56a4947cdc4f1
remainstheday <trentontri@gmail.com>
2022-09-01 21:19:48 -0500

    fix registration with siteId

--- apps/keystone-cms/keystone.ts
+++ apps/keystone-cms/keystone.ts
@@ -1,16 +1,18 @@
+import { createAuth } from "@keystone-6/auth";
+import { config } from "@keystone-6/core";
+import { statelessSessions } from "@keystone-6/core/session";
+
+import bodyParser from "body-parser";
+import express from "express";
+import { Artifact } from "./schema/Artifact";
+import { Comment } from "./schema/Comment";
+import { Experience } from "./schema/Experience";
+import { Site } from "./schema/Site";
+import { SiteContent } from "./schema/SiteContent";
+import { User } from "./schema/User";
 import convertStringToURL from "./utils/convertStringToURL";
 
 require("dotenv").config();
-import { config } from "@keystone-6/core";
-import { createAuth } from "@keystone-6/auth";
-import { statelessSessions } from "@keystone-6/core/session";
-import { Site } from "./schema/Site";
-import { Artifact } from "./schema/Artifact";
-import { User } from "./schema/User";
-import { Experience } from "./schema/Experience";
-import { SiteContent } from "./schema/SiteContent";
-import { Comment } from "./schema/Comment";
-import express from "express";
 
 const { withAuth } = createAuth({
   listKey: "User",
@@ -26,29 +28,25 @@ export default withAuth(
   config({
     server: {
       cors: {
-        origin: [`${process.env.FRONTEND_URL}`, `${process.env.WEBSITE_URL}`],
+        origin: [
+          `${process.env.FRONTEND_URL}`,
+          `${process.env.WEBSITE_URL}`,
+          "http://localhost:3001/register",
+        ],
         credentials: true,
       },
       extendExpressApp: (app, createContext) => {
         app.use(express.json());
-        app.post("/api/newUser", async (req, res, next) => {
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use("/restapi", async (req, res, next) => {
+          (req as any).context = await createContext(req, res);
+          app.use(bodyParser.urlencoded({ extended: true }));
+          app.use(express.json());
           const context = await createContext(req, res);
-          const existingId = await context.prisma.site.findMany({
-            where: {
-              siteId: { equals: `${convertStringToURL(req.body.siteId)}` },
-            },
-          });
           const existingEmail = await context.prisma.user.findUnique({
             where: { email: `${req.body.email}`.toLowerCase() },
           });
 
-          if (existingId.length > 0) {
-            console.log("An organization with that name already exists.");
-
-            return res.status(400).json({
-              error: "An organization with that name already exists.",
-            });
-          }
           if (existingEmail !== null) {
             console.log("A user with that email already exists.");
 
@@ -60,6 +58,63 @@ export default withAuth(
             throw new Error("password must be more than 7 characters");
           }
 
+          /*  Check for existing Site ID before creating a new one */
+          const existingId = await context.prisma.site.findMany({
+            where: {
+              siteId: { equals: `${convertStringToURL(req.body.siteId)}` },
+            },
+          });
+          if (existingId.length > 0) {
+            console.log("An organization with that name already exists.");
+
+            return res.status(400).json({
+              error: "An organization with that name already exists.",
+            });
+          }
+
+          /*  Check for existing URL before creating a new Site */
+          const existingUrl = await context.prisma.site.findMany({
+            where: {
+              url: { equals: `${convertStringToURL(req.body.siteId)}` },
+            },
+          });
+          if (existingUrl.length > 0) {
+            console.log(
+              "An organization with that name or URL already exists."
+            );
+
+            return res.status(400).json({
+              error: "An organization with that name or URL already exists.",
+            });
+          }
+
+          /*  Check for existing URL before creating a new Site */
+          const existingHomepage = await context.prisma.siteContent.findMany({
+            where: {
+              url: { equals: `${convertStringToURL(req.body.siteId)}` },
+            },
+          });
+          const existingAbout = await context.prisma.siteContent.findMany({
+            where: {
+              url: { equals: `${convertStringToURL(req.body.siteId)}/about` },
+            },
+          });
+          if (existingHomepage.length > 0 || existingAbout.length > 0) {
+            console.log(
+              "An organization with that name or URL already exists."
+            );
+
+            return res.status(400).json({
+              error: "An organization with that name or URL already exists.",
+            });
+          }
+          next();
+        });
+
+        app.post("/restapi/newUser", async (req, res) => {
+          app.use(bodyParser.urlencoded({ extended: true }));
+          app.use(express.json());
+          const context = await createContext(req, res);
           const newUser = {
             siteId: `${convertStringToURL(req.body.siteId)}`,
             name: req.body.name,
@@ -67,35 +122,20 @@ export default withAuth(
             password: req.body.password,
             isAdmin: false,
           };
-
           const newSite = {
             siteId: `${convertStringToURL(req.body.siteId)}`,
             title: req.body.siteId,
             url: convertStringToURL(req.body.siteId),
           };
+
           try {
-            await context.query.Site.createOne({
-              data: newSite,
-            });
             await context.query.User.createOne({
               data: newUser,
             });
-            await context.query.SiteContent.createOne({
-              data: {
-                name: "About",
-                title: `About ${newSite.title}`,
-                url: `${newSite.url}/about`,
-                siteId: newSite.siteId,
-              },
+            await context.query.Site.createOne({
+              data: newSite,
             });
-            await context.query.SiteContent.createOne({
-              data: {
-                name: "Home",
-                title: newSite.title,
-                url: newSite.url,
-                siteId: newSite.siteId,
-              },
-            });
+
             return res.json({
               success: true,
             });
