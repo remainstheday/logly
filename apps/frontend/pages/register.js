ed0a7ea7598b7295dee511848b255f507496fead
remainstheday <trentontri@gmail.com>
2022-09-02 00:13:27 -0500

    remove headers

--- apps/frontend/pages/register.js
+++ apps/frontend/pages/register.js
@@ -43,7 +43,6 @@ export default function Register() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify(formData),
     })
